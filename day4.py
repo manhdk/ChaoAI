df336a9cf8d2f8333b089955e46e2a5e2f47af46
ManhDK <manhdk@neoscorp.vn>
2021-01-04 10:03:36 +0700

    add image process

--- day4.py
+++ day4.py
@@ -1,7 +1,22 @@
-# Statistical Summary
-from pandas import read_csv
-url ='https://goo.gl/bDdBiA'
-names = ['preg','plas','pres','skin','test','mass','pedi','age','class']
-data = read_csv(url, names=names)
-description = data.describe()
-print(description)
+# OpenCV
+import cv2 as cv
+print(cv.__version__)
+
+# mot so thao tac xu ly anh
+image_path = "samedata/Dan.jpg"
+
+# Hien thi anh
+image_read = cv.imread(image_path)
+cv.imshow('Hien thi anh', image_read)
+cv.waitKey(0)
+
+# Kiem tra kich thuoc anh
+(h, w, d) = image_read.shape
+print("rong={}, cao={}, sau={}".format(w, h, d))
+
+# Thay doi kich thuoc anh
+(h, w, d) = image_read.shape
+r = 320.0 / w 
+dim = (320, int(h * r))
+resized = cv.resize(image_read, dim)
+cv.imwrite('samedata/Dan1.jpg', resized)
